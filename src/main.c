af68735251979f529414078ff8f68643e11fe345
amengdv <aminkafri0082@gmail.com>
2025-01-20 23:40:07 +0800

    update: skip space unless inside string

--- src/main.c
+++ src/main.c
@@ -12,12 +12,26 @@ int main() {
     }
     char ch;
     int index = 0;
+    int is_inside_string = 0;
+    int string_encounter = 0;
     while ((ch = fgetc(fptr)) != EOF) {
         // Skip spaces so that our array is clean
-        if (ch == ' ' || ch == '\t' || ch == '\n') {
+        // TODO: Unless a string
+
+        if (ch == '"' && string_encounter < 2) {
+            is_inside_string = 1;
+            string_encounter++;
+        } else if (string_encounter == 2) {
+            is_inside_string = 0;
+            string_encounter = 0;
+        }
+        if ((ch == ' ' || ch == '\t' || ch == '\n') && is_inside_string == 0) {
             continue;
         }
         content_buffer[index] = ch;
+        printf("Current char: %c\n", ch);
+        printf("String encountered: %d\n", string_encounter);
+        printf("Inside string: %d\n", is_inside_string);
         index++;
     }
     fclose(fptr);
@@ -31,4 +45,5 @@ int main() {
     printf("Test num 1: %d\n", is_valid_json_numbers(num_test));
     return 0;
 }
+
 
