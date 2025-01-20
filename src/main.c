47ddde03bc862b8b9d2f860953efbc71709625e9
amengdv <aminkafri0082@gmail.com>
2025-01-20 23:17:27 +0800

    update: 4 indents

--- src/main.c
+++ src/main.c
@@ -2,33 +2,33 @@
 #include "yeson.h"
 
 int main() {
-  FILE *fptr;
-  char content_buffer[64];
-  char *filename = "yeson-true.txt";
-  fptr = fopen(filename, "r");
-  if (fptr == NULL) {
-    printf("Error reading from file named %s", filename);
-    return 1;
-  }
-  char ch;
-  int index = 0;
-  while ((ch = fgetc(fptr)) != EOF) {
-    // Skip spaces so that our array is clean
-    if (ch == ' ' || ch == '\t' || ch == '\n') {
-      continue;
+    FILE *fptr;
+    char content_buffer[64];
+    char *filename = "yeson-true.txt";
+    fptr = fopen(filename, "r");
+    if (fptr == NULL) {
+        printf("Error reading from file named %s", filename);
+        return 1;
     }
-    content_buffer[index] = ch;
-    index++;
-  }
-  fclose(fptr);
+    char ch;
+    int index = 0;
+    while ((ch = fgetc(fptr)) != EOF) {
+        // Skip spaces so that our array is clean
+        if (ch == ' ' || ch == '\t' || ch == '\n') {
+            continue;
+        }
+        content_buffer[index] = ch;
+        index++;
+    }
+    fclose(fptr);
   
-  // Testing Place
-  printf("%s\n", content_buffer);
-  char *str_test = "\"Hel\\\"lo\"";
-  printf("%s\n", str_test);
-  printf("%d\n", is_valid_json_string(str_test));
-  char *num_test = "0.e6";
-  printf("Test num 1: %d\n", is_valid_json_numbers(num_test));
-  return 0;
+    // Testing Place
+    printf("%s\n", content_buffer);
+    char *str_test = "\"Hel\\\"lo\"";
+    printf("%s\n", str_test);
+    printf("%d\n", is_valid_json_string(str_test));
+    char *num_test = "0.e6";
+    printf("Test num 1: %d\n", is_valid_json_numbers(num_test));
+    return 0;
 }
 
--- src/yeson.c
+++ src/yeson.c
@@ -6,55 +6,55 @@
 int is_digit_after_decimal(char *str);
 
 int is_valid_json_string(char *str) {
-  size_t str_len = strlen(str);
-  if (str[0] != '"' && str[str_len - 1] != '"') {
-    return 0;
-  }
-  for (int i = 1; i < (int)str_len - 1; ++i) {
-    char curr_str = str[i];
-    char after = str[i + 1];
-    if (curr_str == '"') {
-      return 0;
+    size_t str_len = strlen(str);
+    if (str[0] != '"' && str[str_len - 1] != '"') {
+        return 0;
     }
-    if(curr_str == '\\') {
-      printf("%c\n", after);
-      if (after == 't' || after == 'n' || after == 'b'
-          || after == 'f' || after == 'r' || after == '"' || after == '\\') {
-        i++;
-        continue;
-      }
+    for (int i = 1; i < (int)str_len - 1; ++i) {
+        char curr_str = str[i];
+        char after = str[i + 1];
+        if (curr_str == '"') {
+            return 0;
+        }
+        if(curr_str == '\\') {
+            printf("%c\n", after);
+            if (after == 't' || after == 'n' || after == 'b'
+                || after == 'f' || after == 'r' || after == '"' || after == '\\') {
+                i++;
+                continue;
+            }
+        }
     }
-  }
-  return 1;
+    return 1;
 }
 
 /*
   Expect the string to not have whitespace
- */
+*/
 int is_valid_json_numbers(char *str) {
-  if (str[0] == '+' || ((str[0] < '0' || str[0] > '9') && str[0] != '-')
-      || (str[0] == '0' && strlen(str) > 1 && str[1] != '.')
-      || is_digit_after_decimal(str) == 0) {
-    return 0;
-  }
-  char *end_ptr;
-  strtod(str, &end_ptr);
-  if (str == end_ptr) {
-    return 0;
-  }
-  if (*end_ptr != '\0') {
-    return 0;
-  }
-  return 1;
+    if (str[0] == '+' || ((str[0] < '0' || str[0] > '9') && str[0] != '-')
+        || (str[0] == '0' && strlen(str) > 1 && str[1] != '.')
+        || is_digit_after_decimal(str) == 0) {
+        return 0;
+    }
+    char *end_ptr;
+    strtod(str, &end_ptr);
+    if (str == end_ptr) {
+        return 0;
+    }
+    if (*end_ptr != '\0') {
+        return 0;
+    }
+    return 1;
 }
 
 int is_digit_after_decimal(char *str) {
-  for (size_t i = 0; i < strlen(str); ++i) {
-    if (str[i] == '.') {
-      if (str[i + 1] < '0' || str[i + 1] > '9') {
-        return 0;
-      }
+    for (size_t i = 0; i < strlen(str); ++i) {
+        if (str[i] == '.') {
+            if (str[i + 1] < '0' || str[i + 1] > '9') {
+                return 0;
+            }
+        }
     }
-  }
-  return 1;
+    return 1;
 }
